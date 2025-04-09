76e802b4601b3edc79e9c006f2df2c1eb3e5a9e2
Danka <danielkarosas@gmail.com>
2025-04-09 15:44:02 -0400

    sword fully functional

--- scripts/sword.gd
+++ scripts/sword.gd
@@ -11,12 +11,15 @@ extends Node3D
 @onready var timer = $Timer
 @export var Cool_down = 1.0
 @export var damage: float = 25.0
-
+@onready var hitArea = $"Sword Pivot/HitArea"
 var player : Node3D
+
+
+
 
 func _ready():
     
-   
+    hitArea.monitoring = false
     if enemy:
         print("enemy is lols")
     timer.set_wait_time(Cool_down)
@@ -25,12 +28,21 @@ func _ready():
 func _physics_process(delta: float) -> void:
     if enemy:
         print("enemy is lols FIZEEEEEEEEEK")
+        
+        
+        
+
+        
+        
+        
 func sword_swing():
     
     
     
     if can_attack:
+        hitArea.monitoring = true
         $AnimationPlayer.play("swing")
+        
         can_attack = false
         timer.start()
     else:
@@ -45,6 +57,6 @@ func _on_timer_timeout() -> void:
 
 func _on_hit_area_body_entered(body: Node3D) -> void:
     
-    if body.is_in_group('enemy') and body.has_method("take_damage"):
+    if body.is_in_group('enemy'): #and body.has_method("take_damage"):
         body.take_damage(damage)
        
